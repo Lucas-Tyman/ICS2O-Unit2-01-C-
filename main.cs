f7a3a61e9ffe6ff70cf676e76f786ee95c58fbfc
Lucas-Tyman <99354994+Lucas-Tyman@users.noreply.github.com>
2022-03-09 18:20:53 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,15 @@
+// Created by: Lucas Tyman
+// Created on: Mar 2022
+//
+// This program displays, "Hello, World!"
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("\nDone.");
+    }
+}
